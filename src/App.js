eca74ccf10b22aaeb3a3829be9dc6b9d60801ec4
JoeBuydemDips <ksarkodie111@icloud.com>
2022-02-28 17:50:31 -0500

    Build app.js to display jobs.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,63 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+const url = "https://course-api.com/react-tabs-project";
+
 function App() {
-  return <h2>tabs project setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h2>loading...</h2>
+      </section>
+    );
+  }
+
+  //This is being destructured after the loading.
+  //Because the jobs array wont be an empty array
+  const { company, dates, duties, title } = jobs[value];
+
+  return (
+    <section className="section">
+      <header className="title">
+        <h2>experience</h2>
+        <div class="underline"></div>
+      </header>
+      <div className="jobs-center">
+        {/*btn container */}
+        {/*job info */}
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon" />
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+    </section>
+  );
 }
 
-export default App
+export default App;
